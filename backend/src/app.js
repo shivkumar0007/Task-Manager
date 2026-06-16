d5b233b29a832a0d9b54100fecbfb108b49a4bb5
Shiv <186371286+shivkumar0007@users.noreply.github.com>
2026-06-16 17:28:51 +0530

    implement note creation endpoint and connect to notes database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,19 @@
 import express from "express";
+import noteModel from "./models/note.model.js";
 
 const app= express();
+app.use(express.json());
+
+app.post("/notes", async (req,res)=>{
+  const data = req.body;
+  
+ const note = await noteModel.create(data)
+
+  res.status(201).json({
+    message:"note created",
+    note
+  })
+})
 
 
 
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
 
-    await mongoose.connect("mongodb://localhost:27017/");
+    await mongoose.connect("mongodb://localhost:27017/notes");
     console.log("data base connect");
     
 }
