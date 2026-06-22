93ae8551e5b5321f587bb7827db9e00208564bc8
Shiv <186371286+shivkumar0007@users.noreply.github.com>
2026-06-22 13:23:32 +0530

    remove backend server and related files
    - Changed package type from module to commonjs in package.json
    - Deleted server.js, app.js, auth.controller.js, db.js, user.model.js, and auth.route.js files
    - This refactor removes the server setup and authentication logic from the backend

--- backend/src/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authRouter from "./routes/auth.route.js";
-
-const app = express();
-app.use(express.json);
-
-app.use("api/auth", authRouter);
-
-export default app;
